67f65bda984c2bf986b9b40990b6febf8dfb61f6
Zixin Luo <luozixin@sensetime.com>
2016-05-09 19:06:02 +0800

    add data_output_layer

--- include/caffe/data_layers.hpp
+++ include/caffe/data_layers.hpp
@@ -502,6 +502,34 @@ class FileDataLayer : public Layer<Dtype> {
   int chn_;
   vector<int> view_ids_;
 };
+// Added 2015/10/29
+template <typename Dtype>
+class DataOutputLayer : public Layer<Dtype> {
+ public:
+  explicit DataOutputLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {};
+  virtual ~DataOutputLayer() {};
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "DataOutput"; }
+  virtual inline int MinNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 0; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)
+  { NOT_IMPLEMENTED; };
+
+  std::vector<std::string> file_name;
+  std::vector<int> bottom_num;
+  std::vector<int> bottom_num_stride;
+  DataOutputParameter_FileType output_type;
+};
 
 }  // namespace caffe
 
